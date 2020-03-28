c8cb2bc08e8fc559156cd8687b9b528b6ecf579d
Black Hat <bhat@encom.eu.org>
2020-03-27 17:01:17 -0700

    Reformat.

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,8 @@ use std::sync::Arc;
 
 mod rmp;
 
-#[derive(Clone)]
-struct Score {
-    pub quality: Option<f32>,
-    pub quality_yr: Option<f32>,
-}
-
-#[derive(Clone)]
-struct Professor {
-    pub rmp_id: u32,
-
-    pub score: Option<Score>,
-
-    pub first_name: String,
-    pub last_name: String,
-    pub full_name: String,
-
-    pub department: String,
-}
-
 struct AppState {
-    rmp_graphql_token: Mutex<Option<String>>,
-    name_id_map: Mutex<HashMap<String, Vec<u32>>>,
-    id_professor_map: Mutex<HashMap<u32, Arc<Mutex<Professor>>>>,
+    rmp_controller: rmp::Controller,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -68,189 +47,61 @@ async fn version() -> impl Responder {
     web::Json(json!({"version": "0.0.1"}))
 }
 
-async fn name_to_professor(name: String, data: web::Data<AppState>) -> Option<Arc<Mutex<Professor>>> {
-    let name = name.to_lowercase();
-
-    let mut id_opt: Option<u32> = None;
-
-    let mut name_id_map = data.name_id_map.lock().await;
-    let mut id_professor_map = data.id_professor_map.lock().await;
-
-    if let Some(ids) = name_id_map.get(name.as_str()) {
-        id_opt = ids.get(0).cloned();
-    } else {
-        let res = rmp::search_professor(name.as_str()).await.ok()?;
-
-        let ids: Vec<u32> = res.iter()
-            .map(|r| &r.id)
-            .map(|r| r.replace("teacher:", ""))
-            .map(|r| r.parse::<u32>().ok())
-            .filter_map(|r| r)
-            .collect();
-
-        id_opt = ids.get(0).cloned();
-
-        name_id_map.insert(name, ids);
-
-        for pr in res {
-            if let Ok(id) = pr.id.replace("teacher:", "").parse::<u32>() {
-                if !id_professor_map.contains_key(&id) {
-                    id_professor_map.insert(
-                        id,
-                        Arc::new(
-                            Mutex::new(
-                                Professor {
-                                    rmp_id: id,
-                                    score: None,
-                                    first_name: pr.first_name,
-                                    last_name: pr.last_name,
-                                    full_name: pr.full_name,
-                                    department: pr.department,
-                                }
-                            )
-                        ),
-                    );
-                }
-            }
-        }
-    }
-
-    if let Some(id) = id_opt {
-        return id_professor_map.get(&id).cloned();
-    }
-
-    None
-}
-
-fn get_weighted_score(data: &Vec<rmp::Rating>, offset: u64) -> (f32, f32) {
-    let mut quality_ratings_sum = 0.0;
-    let mut total_weight = 0.0;
-
-    let offsetted = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() - offset;
-
-    for r in data {
-        if (r.date.timestamp() as u64) < offsetted {
-            continue;
-        }
-
-        let quality = (r.helpful + r.clarity) as f32 / 2.0;
-
-        let thumbs_weight = (r.thumbs_up + 1) as f32 / (r.thumbs_up + r.thumbs_down + 1) as f32;
-        let time_weight = ((r.date.timestamp() as u64 - offsetted) as f64 / offset as f64) as f32;
-        let quantity_weight = ((r.thumbs_up + r.thumbs_down) as f32 / 2.0).ln_1p() + 1.0;
-
-        let avg_weight = thumbs_weight * time_weight * quantity_weight;
-
-        total_weight += avg_weight;
-        quality_ratings_sum += quality * avg_weight;
-    }
-
-    (quality_ratings_sum, total_weight)
-}
-
 async fn professor_overview(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
-    if let Some(pr) = name_to_professor(path.clone(), data).await {
-        let mut professor = pr.lock().await;
-        let p = professor.clone();
+    if let Some(pr) = data.rmp_controller.professor_overview(path.clone()).await {
+        let professor = pr.lock().await;
+        let p: rmp::Professor = professor.clone();
 
-        if let Some(score) = professor.score.clone() {
-            return actix_web::Either::A(web::Json(ProfessorResponse {
-                rmp_id: p.rmp_id,
-                quality: p.score.as_ref().map(|e| e.quality).flatten(),
-                quality_yr: p.score.as_ref().map(|e| e.quality_yr).flatten(),
-                first_name: p.first_name,
-                last_name: p.last_name,
-                full_name: p.full_name,
-                department: p.department,
-            }));
-        }
-
-        if let Ok(resp) = rmp::get_professor_comments(professor.rmp_id, None).await {
-            let (score, weight) = get_weighted_score(&resp, 157680000);
-            let (score_yr, weight_yr) = get_weighted_score(&resp, 31536000);
-
-            let professor_score = Score {
-                quality: if weight < 8.0 { None } else { Some(score / weight) },
-                quality_yr: if weight_yr < 2.0 { None } else { Some(score_yr / weight_yr) },
-            };
-
-            professor.score = Some(professor_score.clone());
-
-            return actix_web::Either::A(web::Json(ProfessorResponse {
-                rmp_id: p.rmp_id,
-                quality: professor_score.quality,
-                quality_yr: professor_score.quality_yr,
-                first_name: p.first_name,
-                last_name: p.last_name,
-                full_name: p.full_name,
-                department: p.department,
-            }));
-        }
+        return actix_web::Either::A(web::Json(ProfessorResponse {
+            rmp_id: p.rmp_id,
+            quality: p.score.as_ref().map(|e| e.quality).flatten(),
+            quality_yr: p.score.as_ref().map(|e| e.quality_yr).flatten(),
+            first_name: p.first_name,
+            last_name: p.last_name,
+            full_name: p.full_name,
+            department: p.department,
+        }));
     }
 
     actix_web::Either::B(web::Json(json!({"error": "RMP"})))
 }
 
 async fn professor_comments(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
-    if let Some(pr) = name_to_professor(path.clone(), data).await {
-        let professor = pr.lock().await;
+    let comments: Vec<Comment> = data.rmp_controller.professor_comments(path.clone(), None).await
+        .iter()
+        .map(|r| Comment {
+            class: r.class.clone(),
+            comment: r.comment.replace("&quot;", "\""),
+            grade: r.grade.clone(),
+            attendance_mandatory: r.attendance_mandatory.clone(),
+            quality: (r.clarity + r.helpful) as f32 / 2.0,
+            difficulty: r.difficulty as f32,
+            date: r.date.clone(),
+        })
+        .collect();
 
-        if let Ok(resp) = rmp::get_professor_comments(professor.rmp_id, None).await {
-            let comments: Vec<Comment> = resp.iter()
-                .map(|r| Comment {
-                    class: r.class.clone(),
-                    comment: r.comment.replace("&quot;", r#"""#),
-                    grade: r.grade.clone(),
-                    attendance_mandatory: r.attendance_mandatory.clone(),
-                    quality: (r.clarity + r.helpful) as f32 / 2.0,
-                    difficulty: r.difficulty as f32,
-                    date: r.date.clone(),
-                })
-                .collect();
-
-            return actix_web::Either::A(web::Json(comments));
-        }
-    }
-
-    actix_web::Either::B(web::Json(json!({"error": "RMP"})))
+    return web::Json(comments);
 }
 
 async fn professor_course_comments(path: web::Path<(String, String)>, data: web::Data<AppState>) -> impl Responder {
-    if let Some(pr) = name_to_professor(path.0.clone(), data).await {
-        let professor = pr.lock().await;
+    let comments: Vec<Comment> = data.rmp_controller.professor_comments(path.0.clone(), Some(path.1.clone())).await
+        .iter()
+        .map(|r| Comment {
+            class: r.class.clone(),
+            comment: r.comment.replace("&quot;", "\""),
+            grade: r.grade.clone(),
+            attendance_mandatory: r.attendance_mandatory.clone(),
+            quality: (r.clarity + r.helpful) as f32 / 2.0,
+            difficulty: r.difficulty as f32,
+            date: r.date.clone(),
+        })
+        .collect();
 
-        if let Ok(resp) = rmp::get_professor_comments(professor.rmp_id, Some(path.1.clone())).await {
-            let comments: Vec<Comment> = resp.iter()
-                .map(|r| Comment {
-                    class: r.class.clone(),
-                    comment: r.comment.replace("&quot;", "\""),
-                    grade: r.grade.clone(),
-                    attendance_mandatory: r.attendance_mandatory.clone(),
-                    quality: (r.clarity + r.helpful) as f32 / 2.0,
-                    difficulty: r.difficulty as f32,
-                    date: r.date.clone(),
-                })
-                .collect();
-
-            return actix_web::Either::A(web::Json(comments));
-        }
-    }
-
-    actix_web::Either::B(web::Json(json!({"error": "RMP"})))
+    return web::Json(comments);
 }
 
 async fn rmp_graphql_token(data: web::Data<AppState>) -> impl Responder {
-    if let Some(token) = data.rmp_graphql_token.lock().await.clone() {
-        return web::Json(json!({
-            "token": token,
-        }));
-    }
-
-    if let Ok(token) = rmp::get_rmp_graphql_token().await {
-        let mut rmp_graphql_token = data.rmp_graphql_token.lock().await;
-        *rmp_graphql_token = Some(token.clone());
-        println!("/internal/rmp_graphql_token: cached {}", token);
-
+    if let Ok(token) = data.rmp_controller.graphql_token().await {
         return web::Json(json!({
             "token": token,
         }));
@@ -264,9 +115,7 @@ async fn rmp_graphql_token(data: web::Data<AppState>) -> impl Responder {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let app_state = web::Data::new(AppState {
-        rmp_graphql_token: Mutex::new(None),
-        name_id_map: Mutex::new(HashMap::new()),
-        id_professor_map: Mutex::new(HashMap::new()),
+        rmp_controller: rmp::Controller::new(),
     });
 
     HttpServer::new(move || {
--- src/rmp.rs
+++ src/rmp.rs
@@ -1,10 +1,14 @@
 use serde::{Serialize, Deserialize};
 use failure::Fail;
 use regex::Regex;
-use futures::{TryFutureExt};
+use futures::TryFutureExt;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-const SOLR_QUERY: &'static str = 
-"https://solr-aws-elb-production.ratemyprofessors.com/solr/rmp/select\
+const SOLR_QUERY: &'static str =
+    "https://solr-aws-elb-production.ratemyprofessors.com/solr/rmp/select\
 ?rows=200\
 &wt=json\
 &fq=schoolid_s:1077\
@@ -19,13 +23,13 @@ const SOLR_QUERY: &'static str =
 &q=";
 
 const GRAPHQL_TOKEN_URL: &'static str =
-"https://www.ratemyprofessors.com/ShowRatings.jsp?tid=10000";
+    "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=10000";
 
 const GRAPHQL_URL: &'static str =
-"https://www.ratemyprofessors.com/graphql";
+    "https://www.ratemyprofessors.com/graphql";
 
-const GRAPHQL_QUERY: &'static str = 
-r#"query RatingsListQuery(
+const GRAPHQL_QUERY: &'static str =
+    r#"query RatingsListQuery(
     $id: ID!
     $courseFilter: String
 ) {
@@ -150,7 +154,7 @@ mod int_bool {
                     return Ok(Some(true));
                 }
             }
-            return Ok(None)
+            return Ok(None);
         }
         Err(serde::de::Error::invalid_value(
             serde::de::Unexpected::Unsigned(0),
@@ -196,20 +200,20 @@ mod rmp_mandatory {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GraphQLVariables {
+struct GraphQLVariables {
     id: String,
     #[serde(rename = "courseFilter")]
     course_filter: Option<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GraphQLRequest {
+struct GraphQLRequest {
     pub query: String,
     pub variables: GraphQLVariables,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ProfessorResponse {
+struct ProfessorResponse {
     pub id: String,
     #[serde(rename = "averageratingscore_rf")]
     pub score: Option<f32>,
@@ -224,12 +228,12 @@ pub struct ProfessorResponse {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct DocListResponse {
+struct DocListResponse {
     pub docs: Vec<ProfessorResponse>
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GroupResponse {
+struct GroupResponse {
     #[serde(rename = "groupValue")]
     pub group_name: String,
     #[serde(rename = "doclist")]
@@ -237,18 +241,18 @@ pub struct GroupResponse {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InnerGroupedResponse {
+struct InnerGroupedResponse {
     pub groups: Vec<GroupResponse>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GroupedResponse {
+struct GroupedResponse {
     #[serde(rename = "content_type_s")]
     pub inner: InnerGroupedResponse,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct RMPResponse {
+struct RMPResponse {
     pub grouped: GroupedResponse,
 }
 
@@ -293,80 +297,268 @@ pub struct Rating {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InnerCommentsRatingsEdgesResponse {
+struct InnerCommentsRatingsEdgesResponse {
     pub node: Rating,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InnerCommentsRatingsResponse {
+struct InnerCommentsRatingsResponse {
     pub edges: Vec<InnerCommentsRatingsEdgesResponse>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InnerCommentsNodeResponse {
+struct InnerCommentsNodeResponse {
     pub ratings: InnerCommentsRatingsResponse,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InnerCommentsDataResponse {
+struct InnerCommentsDataResponse {
     pub node: InnerCommentsNodeResponse,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CommentsResponse {
+struct CommentsResponse {
     pub data: InnerCommentsDataResponse,
 }
 
-pub async fn get_rmp_graphql_token() -> Result<String, Error> {
-    let resp = reqwest::get(GRAPHQL_TOKEN_URL)
-        .and_then(|r| async move { r.text().await }).map_err(|_| Error::RMP).await?;
-
-    let re = Regex::new("\"REACT_APP_GRAPHQL_AUTH\":\"(.*?)\"").map_err(|_| Error::RMP)?;
-
-    if let Some(cap) = re.captures_iter(resp.as_str()).next() {
-        return Ok(cap[1].to_string());
-    }
-
-    Err(Error::RMP)
+#[derive(Debug, Clone)]
+pub struct Score {
+    pub quality: Option<f32>,
+    pub quality_yr: Option<f32>,
 }
 
+#[derive(Clone)]
+pub struct Professor {
+    pub rmp_id: u32,
 
-pub async fn search_professor(name: &str) -> Result<Vec<ProfessorResponse>, Error> {
-    let resp = reqwest::get((SOLR_QUERY.to_owned() + name).as_str())
-        .and_then(|r| async move { r.json::<RMPResponse>().await })
-        .map_err(|e| { println!("get_professor_overview: error {}", e); Error::RMP}).await?;
+    pub score: Option<Score>,
 
-    let grouped: Vec<GroupResponse> = resp.grouped.inner.groups;
+    pub first_name: String,
+    pub last_name: String,
+    pub full_name: String,
 
-    if let Some(teacher_group) = grouped.iter()
-        .filter(|g| g.group_name == "TEACHER")
-        .next() {
-        return Ok(teacher_group
-            .doc_list
-            .docs
-            .clone());
-    }
-
-    Ok(Vec::new())
+    pub department: String,
 }
 
-pub async fn get_professor_comments(rmp_id: u32, course: Option<String>) -> Result<Vec<Rating>, Error> {
-    let resp: CommentsResponse = reqwest::Client::builder().build().unwrap()
-        .post(GRAPHQL_URL)
-        .json(&GraphQLRequest {
-            query: GRAPHQL_QUERY.to_owned(),
-            variables: GraphQLVariables {
-                id: base64::encode(format!("Teacher-{}", rmp_id).as_str()),
-                course_filter: course
+struct ControllerData {
+    rmp_graphql_token: Option<String>,
+    name_id_map: HashMap<String, Vec<u32>>,
+    id_professor_map: HashMap<u32, Arc<Mutex<Professor>>>,
+}
+
+pub struct Controller {
+    data: Arc<Mutex<ControllerData>>,
+
+    client: reqwest::Client,
+}
+
+impl Controller {
+    pub fn new() -> Self {
+        let controller_data = ControllerData {
+            rmp_graphql_token: None,
+            name_id_map: HashMap::new(),
+            id_professor_map: HashMap::new(),
+        };
+
+        Controller {
+            data: Arc::new(Mutex::new(controller_data)),
+            client: reqwest::Client::default(),
+        }
+    }
+
+    pub async fn graphql_token(&self) -> Result<String, Error> {
+        {
+            let data = self.data.lock().await;
+
+            if let Some(token) = data.rmp_graphql_token.clone() {
+                return Ok(token);
             }
-        })
-        .header(reqwest::header::AUTHORIZATION, "Basic dGVzdDp0ZXN0")
-        .send()
-        .and_then(|r| async move { r.json::<CommentsResponse>().await }).map_err(|e| { println!("{:?}", e); Error::RMP }).await?;
+        }
 
-    let ratings = resp.data.node.ratings.edges.iter()
-        .map(|r| r.node.clone())
-        .collect();
+        let resp = self.client.get(GRAPHQL_TOKEN_URL)
+            .send()
+            .and_then(|r| async move { r.text().await }).map_err(|_| Error::RMP).await?;
 
-    Ok(ratings)
+        let re = Regex::new("\"REACT_APP_GRAPHQL_AUTH\":\"(.*?)\"").map_err(|_| Error::RMP)?;
+
+        if let Some(cap) = re.captures_iter(resp.as_str()).next() {
+            let token = cap[1].to_string();
+
+            let mut data = self.data.lock().await;
+            data.rmp_graphql_token = Some(token.clone());
+
+            return Ok(token);
+        }
+
+        Err(Error::RMP)
+    }
+
+    pub async fn professor_overview(&self, name: String) -> Option<Arc<Mutex<Professor>>> {
+        if let Some(pr) = self._name_to_professor(name).await {
+            let professor_lock = pr.clone();
+            let mut professor = professor_lock.lock().await;
+
+            if let Some(score) = professor.score.clone() {
+                return Some(pr);
+            }
+
+            if let Ok(resp) = self._professor_comments(professor.rmp_id, None).await {
+                let (score, weight) = Self::_weighted_score(&resp, 157680000);
+                let (score_yr, weight_yr) = Self::_weighted_score(&resp, 31536000);
+
+                let professor_score = Score {
+                    quality: if weight < 8.0 { None } else { Some(score / weight) },
+                    quality_yr: if weight_yr < 2.0 { None } else { Some(score_yr / weight_yr) },
+                };
+
+                professor.score = Some(professor_score.clone());
+
+                return Some(pr);
+            }
+        }
+
+        None
+    }
+
+    pub async fn professor_comments(&self, name: String, course: Option<String>) -> Vec<Rating> {
+        if let Some(pr) = self._name_to_professor(name).await {
+            let professor = pr.lock().await;
+
+            if let Ok(ratings) = self._professor_comments(professor.rmp_id, course).await {
+                return ratings;
+            }
+        }
+
+        Vec::new()
+    }
+
+    async fn _name_to_professor(&self, name: String) -> Option<Arc<Mutex<Professor>>> {
+        let mut data = self.data.lock().await;
+
+        let name = name.to_lowercase();
+        let mut id_opt: Option<u32> = None;
+
+        if let Some(ids) = data.name_id_map.get(name.as_str()) {
+            id_opt = ids.get(0).cloned();
+        } else {
+            let res = self._search_professor(name.as_str()).await.ok()?;
+
+            let ids: Vec<u32> = res.iter()
+                .map(|r| &r.id)
+                .map(|r| r.replace("teacher:", ""))
+                .map(|r| r.parse::<u32>().ok())
+                .filter_map(|r| r)
+                .collect();
+
+            id_opt = ids.get(0).cloned();
+
+            data.name_id_map.insert(name, ids);
+
+            for pr in res {
+                if let Ok(id) = pr.id.replace("teacher:", "").parse::<u32>() {
+                    if !data.id_professor_map.contains_key(&id) {
+                        data.id_professor_map.insert(
+                            id,
+                            Arc::new(
+                                Mutex::new(
+                                    Professor {
+                                        rmp_id: id,
+                                        score: None,
+                                        first_name: pr.first_name,
+                                        last_name: pr.last_name,
+                                        full_name: pr.full_name,
+                                        department: pr.department,
+                                    }
+                                )
+                            ),
+                        );
+                    }
+                }
+            }
+        }
+
+        if let Some(id) = id_opt {
+            return data.id_professor_map.get(&id).cloned();
+        }
+
+        None
+    }
+
+    async fn _search_professor(&self, name: &str) -> Result<Vec<ProfessorResponse>, Error> {
+        let resp = self.client.get((SOLR_QUERY.to_owned() + name).as_str())
+            .send()
+            .and_then(|r| async move { r.json::<RMPResponse>().await })
+            .map_err(|e| {
+                println!("get_professor_overview: error {}", e);
+                Error::RMP
+            }).await?;
+
+        let grouped: Vec<GroupResponse> = resp.grouped.inner.groups;
+
+        if let Some(teacher_group) = grouped.iter()
+            .filter(|g| g.group_name == "TEACHER")
+            .next() {
+            return Ok(teacher_group
+                .doc_list
+                .docs
+                .clone());
+        }
+
+        Ok(Vec::new())
+    }
+
+    async fn _professor_comments(&self, rmp_id: u32, course: Option<String>) -> Result<Vec<Rating>, Error> {
+        if let Ok(token) = self.graphql_token().await {
+            let resp: CommentsResponse = self.client
+                .post(GRAPHQL_URL)
+                .json(&GraphQLRequest {
+                    query: GRAPHQL_QUERY.to_owned(),
+                    variables: GraphQLVariables {
+                        id: base64::encode(format!("Teacher-{}", rmp_id).as_str()),
+                        course_filter: course,
+                    },
+                })
+                .header(reqwest::header::AUTHORIZATION, format!("Basic {}", token))
+                .send()
+                .and_then(|r| async move { r.json::<CommentsResponse>().await })
+                .map_err(|e| {
+                    println!("{:?}", e);
+                    Error::RMP
+                }).await?;
+
+            let ratings = resp.data.node.ratings.edges.iter()
+                .map(|r| r.node.clone())
+                .collect();
+
+            return Ok(ratings);
+        }
+
+        Err(Error::RMP)
+    }
+
+    fn _weighted_score(data: &Vec<Rating>, offset: u64) -> (f32, f32) {
+        let mut quality_ratings_sum = 0.0;
+        let mut total_weight = 0.0;
+
+        let offsetted = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() - offset;
+
+        for r in data {
+            if (r.date.timestamp() as u64) < offsetted {
+                continue;
+            }
+
+            let quality = (r.helpful + r.clarity) as f32 / 2.0;
+
+            let thumbs_weight = (r.thumbs_up + 1) as f32 / (r.thumbs_up + r.thumbs_down + 1) as f32;
+            let time_weight = ((r.date.timestamp() as u64 - offsetted) as f64 / offset as f64) as f32;
+            let quantity_weight = ((r.thumbs_up + r.thumbs_down) as f32 / 2.0).ln_1p() + 1.0;
+
+            let avg_weight = thumbs_weight * time_weight * quantity_weight;
+
+            total_weight += avg_weight;
+            quality_ratings_sum += quality * avg_weight;
+        }
+
+        (quality_ratings_sum, total_weight)
+    }
 }
